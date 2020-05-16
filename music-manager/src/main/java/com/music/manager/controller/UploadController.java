b6e8ea85115e9f13f837c3b6ff68fc438f00a2af
wuqiang-ship-it <1370001785@qq.com>
2020-05-16 20:38:04 +0800

    吴强--增加了用户上传或更换头像的功能

---
+++ music-manager/src/main/java/com/music/manager/controller/UploadController.java
@@ -0,0 +1,46 @@
+package com.music.manager.controller;
+
+import com.music.common.result.FileResult;
+import com.music.manager.pojo.User;
+import com.music.manager.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 文件上传Controller
+ *
+ * @author zhoubin
+ * @create 2019/12/26
+ * @since 1.0.0
+ */
+@Controller
+@RequestMapping("fileUpload")
+public class UploadController {
+
+	@Autowired
+	private UploadService uploadService;
+
+	@RequestMapping("save")
+	@ResponseBody
+	private FileResult upload(MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// System.out.println(file.getName());
+		// System.out.println(file.getOriginalFilename());
+		String filename = file.getOriginalFilename();
+		//格式化时间
+		String date = DateTimeFormatter.ofPattern("yyyy/MM/dd/").format(LocalDateTime.now());
+		filename = date + System.currentTimeMillis() + filename.substring(filename.lastIndexOf("."));
+		User user=(User)request.getSession().getAttribute("user");
+		return uploadService.upload(file.getInputStream(), filename,user);
+	}
+
+
+}
---
+++ music-manager/src/main/java/com/music/manager/service/impl/UploadServiceImpl.java
@@ -0,0 +1,90 @@
+package com.music.manager.service.impl;
+
+import com.music.common.result.FileResult;
+import com.music.manager.mapper.UserMapper;
+import com.music.manager.pojo.User;
+import com.music.manager.service.UploadService;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+/**
+ * 上传service实现类
+ *
+ * @author zhoubin
+ * @create 2019/12/26
+ * @since 1.0.0
+ */
+@Service
+public class UploadServiceImpl implements UploadService {
+	@Autowired
+	private UserMapper userMapper;
+	/**
+	 * 上传
+	 * @param inputStream
+	 * @param fileName
+	 * @return
+	 */
+	@Override
+	public FileResult upload(InputStream inputStream, String fileName, User user) {
+		FileResult fileResult = new FileResult();
+		//构造一个带指定 Region 对象的配置类
+		Configuration cfg = new Configuration(Region.region0());
+		//...其他参数参考类注释
+		UploadManager uploadManager = new UploadManager(cfg);
+		//...生成上传凭证，然后准备上传
+		String accessKey = "gpWL7rHISpMzq7yWddEN_WRUk8UnubuupNthcC3Q";
+		String secretKey = "jez1pZYt-5hO1QwRIb9E4VcwqG3GtJSIfgx4IoAr";
+		String bucket = "music-head-img";
+		//默认不指定key的情况下，以文件内容的hash值作为文件名
+		String key = fileName;
+		try {
+			Auth auth = Auth.create(accessKey, secretKey);
+			String upToken = auth.uploadToken(bucket);
+			try {
+				Response response = uploadManager.put(inputStream,key,upToken,null, null);
+				//解析上传成功的结果
+				// DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+				// System.out.println(putRet.key);
+				// System.out.println(putRet.hash);
+				if (response.statusCode==200){
+					if(user==null||user.equals("")){
+						fileResult.setError("error");
+						fileResult.setMessage("用户未登录");
+						return fileResult;
+					}
+					fileResult.setSuccess("success");
+					fileResult.setMessage("上传成功");
+					fileResult.setFileUrl("http://qafbatitv.bkt.clouddn.com/"+fileName);
+					user.setHeadImage("http://qafbatitv.bkt.clouddn.com/"+fileName);
+					userMapper.updateByPrimaryKey(user);
+					return fileResult;
+				}else {
+					fileResult.setError("error");
+					fileResult.setMessage("上传失败");
+					return fileResult;
+				}
+			} catch (QiniuException ex) {
+				Response r = ex.response;
+				System.err.println(r.toString());
+				fileResult.setError("error");
+				fileResult.setMessage("上传失败");
+				try {
+					System.err.println(r.bodyString());
+				} catch (QiniuException ex2) {
+					//ignore
+				}
+			}
+		} catch (Exception ex) {
+			//ignore
+		}
+		return fileResult;
+	}
+}
